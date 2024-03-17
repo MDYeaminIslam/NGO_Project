ab09503b3ee18a1e75114f5e8471a023e15fc326
joyhasan08 <rubayedhasan235@gmail.com>
2024-03-18 01:19:50 +0600

    NGO init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-5xl text-center'>
+        NGO Project
+      </div>
+    </>
+  )
+}
+
+export default App
