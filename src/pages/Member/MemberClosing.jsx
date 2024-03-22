0fc13cc92611d845c64eb1be1476d04e745d4386
Al Ifran <ifran15-2915@diu.edu.bd>
2024-03-23 01:14:50 +0600

    Member closing & Member request completedd

--- src/pages/Member/MemberClosing.jsx
+++ src/pages/Member/MemberClosing.jsx
@@ -4,7 +4,35 @@ const MemberClosing = () => {
   return (
     <div>
       <section><MemberNav /></section>
-      <h1>Member Closing</h1>
+      <section className="m-4">
+        <h1 className="text-xl font-bold text-start max-w-5xl mx-auto  pt-4 border-b-4 pb-2 ">Member Closing</h1>
+        <form className="my-8" action="">
+          <section className=" grid grid-cols-2 max-w-5xl mx-auto gap-4 ">
+
+            <div className="flex flex-col gap-1">
+              <label className="font-medium" htmlFor="member_id"  >Member Id: </label>
+              <input className="border-2 hover:border-black rounded " id="member_id" type="text"/>
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <label className="font-medium" htmlFor="closing_date">Closing Date:</label>
+              <input className="border-2 hover:border-black rounded " id="closing_date" type="date" />
+            </div>
+
+            <div className="flex flex-col gap-1 col-span-2">
+              <label className="font-medium" htmlFor="closing_reason">Closing Reason:</label>
+              <textarea id="closing_reason" cols="10" rows="2" className="border-2 hover:border-black rounded "></textarea>
+            </div>
+          </section>
+
+          {/* Submit button */}
+          
+          <div className="w-full flex  pl-3 mt-12 justify-center">
+            <input className="bg-sky-500 hover:bg-sky-600 px-20 py-2 rounded font-medium text-white" type="submit"/>
+          </div>
+        </form>
+
+      </section>
     </div>
   );
 };
--- src/pages/Member/MemberRequest.jsx
+++ src/pages/Member/MemberRequest.jsx
@@ -6,7 +6,76 @@ const MemberRequest = () => {
       <section>
         <MemberNav />
       </section>
-      <h1>Member Request</h1>
+     
+      <section className="m-4">
+        <h1 className="text-xl font-bold text-start max-w-5xl mx-auto  pt-4 border-b-4 pb-2 mb-10">Member Request</h1>
+
+        <p className="mb-3">You have 4 Member request pending!!</p>
+        
+        <div className="flex flex-col">
+          <div className="-m-1.5 overflow-x-auto">
+            <div className="p-1.5 min-w-full inline-block align-middle">
+              <div className="border overflow-hidden border-black">
+
+
+                <table className="min-w-full divide-y divide-gray-200">
+
+                  <thead className="bg-slate-800">
+
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-start text-xs font-bold text-white uppercase">Name</th>
+                      
+                      <th scope="col" className="px-6 py-3 text-start text-xs font-bold text-white uppercase">ID</th>
+                      <th scope="col" className="px-6 py-3 text-center text-xs font-bold text-white uppercase">Action</th>
+                    </tr>
+
+                  </thead>
+
+                  <tbody className="divide-y divide-gray-200">
+                    <tr>
+                      <td className="px-6 py-4 text-sm font-medium text-black">Dummy text</td>
+                      <td className="px-6 py-4 text-sm text-black">9847562</td>
+                      <td className="px-6 py-4 text-center text-sm font-medium">
+                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-green-500 hover:text-green-700 pr-4">Approved </button>
+                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-red-500 hover:text-red-700">Rejected</button>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td className="px-6 py-4 text-sm font-medium text-black">Dummy text</td>
+                      <td className="px-6 py-4 text-sm text-black">9847562</td>
+                      <td className="px-6 py-4 text-center text-sm font-medium">
+                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-green-500 hover:text-green-700 pr-4">Approved </button>
+                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-red-500 hover:text-red-700">Rejected</button>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td className="px-6 py-4 text-sm font-medium text-black">Dummy text</td>
+                      <td className="px-6 py-4 text-sm text-black">9847562</td>
+                      <td className="px-6 py-4 text-center text-sm font-medium">
+                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-green-500 hover:text-green-700 pr-4">Approved </button>
+                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-red-500 hover:text-red-700">Rejected</button>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td className="px-6 py-4 text-sm font-medium text-black">Dummy text</td>
+                      <td className="px-6 py-4 text-sm text-black">9847562</td>
+                      <td className="px-6 py-4 text-center text-sm font-medium">
+                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-green-500 hover:text-green-700 pr-4">Approved </button>
+                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-red-500 hover:text-red-700">Rejected</button>
+                      </td>
+                    </tr>
+
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+       </div>
+
+      </section>
     </div>
   );
 };
