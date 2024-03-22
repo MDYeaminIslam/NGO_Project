7fa62d7e5f07e3b85c4961ba1bd8e2f3e12aa738
joyhasan08 <rubayedhasan235@gmail.com>
2024-03-23 01:57:19 +0600

    bug fix

--- src/pages/Branches/BranchesNav/BranchesNav.jsx
+++ src/pages/Branches/BranchesNav/BranchesNav.jsx
@@ -4,32 +4,32 @@ const BranchesNav = () => {
   return (
     <div>
       <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="hidden sm:block sm:ml-6">
-            <div className="flex space-x-4">
-              
-              <Link to={'/add_new_branch'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Add New Branch</p>
-              </Link>
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
 
-              <Link to={'/add_new_samity'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Add New Samity</p>
-              </Link>
+                <Link to={'#/add_new_branch'}>
+                  <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Add New Branch</p>
+                </Link>
 
-              <Link to={'/branch_list'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Branch List</p>
-              </Link>
+                <Link to={'/add_new_samity'}>
+                  <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Add New Samity</p>
+                </Link>
 
-              <Link to={'/samity_list'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Samity List</p>
-              </Link>
+                <Link to={'/branch_list'}>
+                  <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Branch List</p>
+                </Link>
 
+                <Link to={'/samity_list'}>
+                  <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Samity List</p>
+                </Link>
+
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </div>
   );
 };
