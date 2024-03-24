1978e8a905768f88a5978fb7c17970f471813351
joyhasan08 <rubayedhasan235@gmail.com>
2024-03-25 03:40:40 +0600

    ui color update

--- src/pages/Member/AddMember.jsx
+++ src/pages/Member/AddMember.jsx
@@ -14,70 +14,70 @@ const AddMember = () => {
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="name"  >Name: </label>
-              <input className="border-2 hover:border-black rounded " id="name" name="name" type="text" />
+              <input className="border-2 rounded hover:border-teal-500 " id="name" name="name" type="text" />
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="fathers_name">Father’s Name:</label>
-              <input className="border-2 hover:border-black rounded " id="fathers_name" type="text" />
+              <input className="border-2 hover:border-teal-500 rounded " id="fathers_name" type="text" />
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="mothers_name">Mother’s Name:</label>
-              <input className="border-2 hover:border-black rounded " id="mothers_name" type="text" />
+              <input className="border-2 hover:border-teal-500 rounded " id="mothers_name" type="text" />
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="spouse_name">Spouse Name:</label>
-              <input className="border-2 hover:border-black rounded " id="spouse_name" type="text" />
+              <input className="border-2 hover:border-teal-500 rounded " id="spouse_name" type="text" />
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="occupation">Occupation:</label>
-              <input className="border-2 hover:border-black rounded " id="occupation" type="text" />
+              <input className="border-2 hover:border-teal-500 rounded " id="occupation" type="text" />
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="religion">Religion:</label>
-              <input className="border-2 hover:border-black rounded " id="religion" type="text" />
+              <input className="border-2 hover:border-teal-500 rounded " id="religion" type="text" />
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="short_brief_of_occupation">Short Brief of Occupation:</label>
-              <textarea name="" id="short_brief_of_occupation" cols="10" rows="2" className="border-2 hover:border-black rounded "></textarea>
+              <textarea name="" id="short_brief_of_occupation" cols="10" rows="2" className="border-2 hover:border-teal-500 rounded "></textarea>
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="present_address"> Present Address:</label>
-              <textarea name="" id=" present_address" cols="10" rows="2" className="border-2 hover:border-black rounded "></textarea>
+              <textarea name="" id=" present_address" cols="10" rows="2" className="border-2 hover:border-teal-500 rounded "></textarea>
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor=" permanent_address">  Permanent Address:</label>
-              <textarea name="" id="  permanent_address" cols="10" rows="2" className="border-2 hover:border-black rounded "></textarea>
+              <textarea name="" id="  permanent_address" cols="10" rows="2" className="border-2 hover:border-teal-500 rounded "></textarea>
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="nid_number">NID Number:</label>
-              <input className="border-2 hover:border-black rounded " id="nid_number" type="text" />
+              <input className="border-2 hover:border-teal-500 rounded " id="nid_number" type="number" />
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="mobile_number">Mobile Number:</label>
-              <input className="border-2 hover:border-black rounded " id="mobile_number" type="text" />
+              <input className="border-2 hover:border-teal-500 rounded " id="mobile_number" type="number" placeholder="+880" />
             </div>
 
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="emergency_number">Emergency Contact Number:</label>
-              <input className="border-2 hover:border-black rounded " id="emergency_number" type="text" />
+              <input className="border-2 hover:border-teal-500 rounded " id="emergency_number" type="number" placeholder="+880" />
             </div>
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="membership_fee ">Membership Fee:</label>
-              <input className="border-2 hover:border-black rounded " id="membership_fee " type="number" />
+              <input className="border-2 hover:border-teal-500 rounded " id="membership_fee " type="number" />
             </div>
             <div className="flex flex-col gap-1">
               <label className="font-medium" htmlFor="Attach_Photo ">Attach Photo</label>
-              <input className="border-2 hover:border-black rounded " id="Attach_Photo " type="file" />
+              <input className="border-2 hover:border-teal-500 rounded " id="Attach_Photo " type="file" />
             </div>
 
           </section>
@@ -88,22 +88,22 @@ const AddMember = () => {
 
               <div className="flex flex-col gap-1">
                 <label className="font-medium" htmlFor="name"  >Name: </label>
-                <input className="border-2 hover:border-black rounded " id="name" type="text" />
+                <input className="border-2 hover:border-teal-500 rounded " id="name" type="text" />
               </div>
 
               <div className="flex flex-col gap-1">
                 <label className="font-medium" htmlFor="relation_with_member"  >Relation with Member: </label>
-                <input className="border-2 hover:border-black rounded " id="relation_with_member" type="text" />
+                <input className="border-2 hover:border-teal-500 rounded " id="relation_with_member" type="text" />
               </div>
 
               <div className="flex flex-col gap-1">
                 <label className="font-medium" htmlFor="share"  >Share % </label>
-                <input className="border-2 hover:border-black rounded " id="share" type="number" />
+                <input className="border-2 hover:border-teal-500 rounded " id="share" type="number" />
               </div>
 
               <div className="flex flex-col gap-1">
                 <label className="font-medium" htmlFor="occupation">Occupation:</label>
-                <input className="border-2 hover:border-black rounded " id="occupation" type="text" />
+                <input className="border-2 hover:border-teal-500 rounded " id="occupation" type="text" />
               </div>
 
             </section>
