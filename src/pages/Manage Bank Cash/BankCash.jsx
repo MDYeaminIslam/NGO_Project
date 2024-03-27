088079f164b1931533780b0acb7362ace90e1c74
Nashitgit <nazmulnashitofficial@gmail.com>
2024-03-28 01:23:11 +0600

    employee and manage bank cash added

--- src/pages/Manage Bank Cash/BankCash.jsx
+++ src/pages/Manage Bank Cash/BankCash.jsx
@@ -4,9 +4,53 @@ const BankCash = () => {
   return (
     <div>
       <section>
-        <ManageBankCashNav/>
+        <ManageBankCashNav />
       </section>
-      <h1>Bank Cash</h1>
+
+      <section className="m-4">
+        <h1 className="text-xl font-bold text-start max-w-5xl mx-auto  pt-4 border-b-4 pb-2 ">Bank Cash </h1>
+        <form className="my-8" >
+          <section className="grid grid-cols-3 max-w-5xl mx-auto gap-4">
+
+            <div className="flex flex-col gap-1">
+              <label className="font-medium" htmlFor="cash_in_amount">Cash In Amount :</label>
+              <input className="input input-bordered input-sm  hover:border-teal-500  " id="cash_in_amount" type="text" placeholder="enter cash in amount" />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <label className="font-medium" htmlFor="cash_out_amount">Cash Out Amount :</label>
+              <input className="input input-bordered input-sm  hover:border-teal-500  " id="cash_out_amount" type="text" placeholder="enter cash out amount" />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <label className="font-medium" htmlFor="bank_name">Bank Name :</label>
+              <input className="input input-bordered input-sm  hover:border-teal-500  " id="bank_name" type="text" placeholder="enter bank name" />
+            </div>
+
+
+            <div className="flex flex-col gap-1 ">
+              <label className="font-medium" htmlFor="date"> Date :</label>
+              <input className="input input-bordered input-sm  hover:border-teal-500  " id="date" type="date" placeholder="" />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <label className="font-medium" htmlFor="source_details">Source Details :</label>
+              <input className="input input-bordered input-sm  hover:border-teal-500  " id="source_details" type="text" placeholder="enter source details" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label className="font-medium" htmlFor="remarks">Remarks :</label>
+              <input className="input input-bordered input-sm  hover:border-teal-500  " id="remarks" type="text" placeholder="" />
+            </div>
+
+
+          </section>
+
+        </form>
+
+        <div className="w-full flex justify-center  m-8">
+          <input className="bg-teal-600 hover:bg-teal-700 px-10 py-2 rounded font-medium     text-white" type="submit" />
+        </div>
+      </section>
     </div>
   );
 };
--- src/pages/Manage Bank Cash/ManageBankCashNav/ManageBankCashNav.jsx
+++ src/pages/Manage Bank Cash/ManageBankCashNav/ManageBankCashNav.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 const ManageBankCashNav = () => {
   return (
     <div>
-      <nav className="bg-gray-800">
+      <nav className="bg-teal-700 text-white">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="hidden sm:block sm:ml-6">
             <div className="flex space-x-4">
               
               <Link to={'/bank_cash'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Bank Cash</p>
+              <p className=" hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Bank Cash</p>
               </Link>
 
               <Link to={'/bank_cash_to_drawer'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Bank Cash to Drawer</p>
+              <p className=" hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Bank Cash to Drawer</p>
               </Link>
 
             </div>
