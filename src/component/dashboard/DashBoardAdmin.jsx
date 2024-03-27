fcf4745233087fcf39b1237f6fdedfd1ff43bb8b
joyhasan08 <rubayedhasan235@gmail.com>
2024-03-28 02:30:00 +0600

    dashboard updated

--- src/component/dashboard/DashBoardAdmin.jsx
+++ src/component/dashboard/DashBoardAdmin.jsx
@@ -2,10 +2,54 @@
 
 const DashBoardAdmin = () => {
     return (
-        <div className="flex justify-center items-center   bg-gray-100 md:py-10 min-h-screen no-scrollbar   ">
+        <div className="flex flex-col gap-10 justify-center items-center   bg-gray-100 md:py-10 min-h-screen no-scrollbar   ">
+
+            <section className="flex gap-6">
+                <div className="flex items-center gap-6">
+                    <div className="avatar">
+                        <div className="w-14 rounded-full ring ring-teal-500 ring-offset-base-100 ring-offset-2">
+                            <img src="https://source.unsplash.com/100x100/?portrait" />
+                        </div>
+                    </div>
+                    <div>
+                        <h1 className="text-lg font-medium">Master Admin</h1>
+                        <p>ID: 50174</p>
+                    </div>
+                </div>
+                <ul className="menu bg-base-300 lg:menu-horizontal items-center  rounded-box">
+                    <li>
+                        <a className="hover:bg-teal-500 hover:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 " fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+                            Notification
+                            <span className="badge badge-sm">99+</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="hover:bg-teal-500 hover:text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                            Updates
+                            <span className="badge badge-sm badge-warning">NEW</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="hover:bg-teal-500 hover:text-white">
+                            Stats
+                            <span className="  badge badge-xs badge-info"></span>
+                        </a>
+                    </li>
+                    <div>
+                        <label className="flex cursor-pointer gap-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+                            <input type="checkbox" value="synthwave" className="toggle theme-controller" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                        </label>
+                    </div>
+                </ul>
+            </section>
+
             <section className="grid grid-cols-2 md:grid-cols-4 md:gap-4 gap-2 w-fit    ">
                 {/* card */}
-                <div className="bg-teal-700 text-white p-4 rounded-md md:col-span-2 ">
+                <div className="bg-teal-700 hover:bg-teal-500 text-white p-4 rounded-md md:col-span-2 ">
                     <div>
                         <img className="w-10" src="/Admin Dashboard icon/Total capital.png" alt="" />
                     </div>
@@ -15,7 +59,7 @@ const DashBoardAdmin = () => {
                     </div>
                 </div>
                 {/* card */}
-                <div className="bg-teal-700 text-white p-4 rounded-md">
+                <div className="bg-teal-700 hover:bg-teal-500 text-white p-4 rounded-md">
                     <div>
                         <img className="w-10" src="/Admin Dashboard icon/Number of center.png  " alt="" />
                     </div>
@@ -25,7 +69,7 @@ const DashBoardAdmin = () => {
                     </div>
                 </div>
                 {/* card */}
-                <div className="bg-teal-700 text-white p-4 rounded-md">
+                <div className="bg-teal-700 hover:bg-teal-500 text-white p-4 rounded-md">
                     <div>
                         <img className="w-10" src="/Admin Dashboard icon/Loan in field.png  " alt="" />
                     </div>
@@ -35,7 +79,7 @@ const DashBoardAdmin = () => {
                     </div>
                 </div>
                 {/* card */}
-                <div className="bg-teal-700 text-white p-4 rounded-md">
+                <div className="bg-teal-700 hover:bg-teal-500 text-white p-4 rounded-md">
                     <div>
                         <img className="w-10" src="/Admin Dashboard icon/Total collection.png " alt="" />
                     </div>
@@ -45,7 +89,7 @@ const DashBoardAdmin = () => {
                     </div>
                 </div>
                 {/* card */}
-                <div className="bg-teal-700 text-white p-4 rounded-md">
+                <div className="bg-teal-700 hover:bg-teal-500 text-white p-4 rounded-md">
                     <div>
                         <img className="w-10" src="/Admin Dashboard icon/Total members.png  " alt="" />
                     </div>
@@ -55,7 +99,7 @@ const DashBoardAdmin = () => {
                     </div>
                 </div>
                 {/* card */}
-                <div className="bg-teal-700 text-white p-4 rounded-md">
+                <div className="bg-teal-700 hover:bg-teal-500 text-white p-4 rounded-md">
                     <div>
                         <img className="w-10" src="/Admin Dashboard icon/Balance in bank.png  " alt="" />
                     </div>
@@ -65,7 +109,7 @@ const DashBoardAdmin = () => {
                     </div>
                 </div>
                 {/* card */}
-                <div className="bg-teal-700 text-white p-4 rounded-md">
+                <div className="bg-teal-700 hover:bg-teal-500 text-white p-4 rounded-md">
                     <div>
                         <img className="w-10" src="/Admin Dashboard icon/drawer cash.png " alt="" />
                     </div>
@@ -75,7 +119,7 @@ const DashBoardAdmin = () => {
                     </div>
                 </div>
                 {/* card */}
-                <div className="bg-teal-700 text-white p-4 rounded-md">
+                <div className="bg-teal-700 hover:bg-teal-500 text-white p-4 rounded-md">
                     <div>
                         <img className="w-10" src="/Admin Dashboard icon/Regular loan.png " alt="" />
                     </div>
@@ -85,7 +129,7 @@ const DashBoardAdmin = () => {
                     </div>
                 </div>
                 {/* card */}
-                <div className="bg-teal-700 text-white p-4 rounded-md">
+                <div className="bg-teal-700 hover:bg-teal-500 text-white p-4 rounded-md">
                     <div>
                         <img className="w-10" src="/Admin Dashboard icon/Irregular loan.png " alt="" />
                     </div>
--- src/pages/Loan Management/LoanManagementNav/LoanManagementNav.jsx
+++ src/pages/Loan Management/LoanManagementNav/LoanManagementNav.jsx
@@ -4,46 +4,46 @@ const LoanManagementNav = () => {
   return (
     <div>
       <nav className="bg-teal-700 text-white">
-      <div className=" px-2  ">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="hidden sm:block ">
-            <div className="flex flex-wrap items-center justify-center gap-1">
-              
-              <Link to={'/open_loan_account'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Open Loan Account</p>
-              </Link>
+        <div className=" px-2  ">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="hidden sm:block ">
+              <div className="flex flex-wrap items-center justify-center gap-1">
 
-              <Link to={'/loan_account_list'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Loan Account List</p>
-              </Link>
+                <Link to={'/open_loan_account'}>
+                  <p className=" hover:bg-gray-700  px-3 py-2 rounded-md text-sm font-medium">Open Loan Account</p>
+                </Link>
 
-              <Link to={'/loan_evaluation'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Loan Evaluation</p>
-              </Link>
+                <Link to={'/loan_account_list'}>
+                  <p className=" hover:bg-gray-700  px-3 py-2 rounded-md text-sm font-medium">Loan Account List</p>
+                </Link>
 
-              <Link to={'/loan_transaction_posting'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Transaction Posting</p>
-              </Link>
+                <Link to={'/loan_evaluation'}>
+                  <p className=" hover:bg-gray-700  px-3 py-2 rounded-md text-sm font-medium">Loan Evaluation</p>
+                </Link>
 
-              <Link to={'/loan_account_closing'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Account Closing</p>
-              </Link>
+                <Link to={'/loan_transaction_posting'}>
+                  <p className=" hover:bg-gray-700  px-3 py-2 rounded-md text-sm font-medium">Transaction Posting</p>
+                </Link>
 
-              <Link to={'/loan_account_request'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Loan Account Request</p>
-              </Link>
+                <Link to={'/loan_account_closing'}>
+                  <p className=" hover:bg-gray-700  px-3 py-2 rounded-md text-sm font-medium">Account Closing</p>
+                </Link>
 
-              <Link to={'/loan_closing_account_request'}>
-              <p className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Closing Account Request</p>
-              </Link>
+                <Link to={'/loan_account_request'}>
+                  <p className=" hover:bg-gray-700  px-3 py-2 rounded-md text-sm font-medium">Loan Account Request</p>
+                </Link>
 
-             
+                <Link to={'/loan_closing_account_request'}>
+                  <p className=" hover:bg-gray-700  px-3 py-2 rounded-md text-sm font-medium">Closing Account Request</p>
+                </Link>
 
+
+
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </div>
   );
 };
