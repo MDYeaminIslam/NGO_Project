72743cbdf8ed2c68549b485a56918dfcb008691e
Nashitgit <nazmulnashitofficial@gmail.com>
2024-04-22 23:47:30 +0600

    select

--- src/pages/Member/MembersList.jsx
+++ src/pages/Member/MembersList.jsx
@@ -29,10 +29,15 @@ const MembersList = () => {
       <section>
         <MemberNav />
       </section>
-      <section>
+      <section className="p-4 grid gap-1 grid-cols-2">
+
         <BranchSamitySelector callBackFn={setFormData} />
-        <button onClick={handleSubmit}>Search</button>
+
       </section>
+      <div className="w-fit mx-auto">
+
+        <button className="bg-teal-600 hover:bg-teal-700 px-20 py-2 rounded font-medium text-white mt-8" onClick={handleSubmit}>Search</button>
+      </div>
       {/* Local User List */}
       <section>
         {localUsers.length
