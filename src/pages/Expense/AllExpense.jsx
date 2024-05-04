8b5ce20d155b678216c2e90129a9609236830986
Al Ifran <ifran15-2915@diu.edu.bd>
2024-05-04 11:05:13 +0600

    changed

--- src/pages/Expense/AllExpense.jsx
+++ src/pages/Expense/AllExpense.jsx
@@ -31,42 +31,48 @@ const AllExpense = () => {
       <section>
         <ExpenseNav />
       </section>
+
       <section className="m-4">
         <h1 className="text-xl font-bold text-start max-w-5xl mx-auto  pt-4 border-b-4 pb-2 ">
           All Expense{" "}
         </h1>
-      </section>
-      <section className="p-4 grid gap-1 grid-cols-3">
-        <BranchSamitySelector callBackFn={setFormData} />
-        <div className="flex flex-col gap-1">
-          <label className="font-medium " htmlFor="payment_term">
-            Expense Type :
-          </label>
-          <select
-            onChange={handleChange}
-            name="type"
-            className=" input input-bordered input-sm hover:border-teal-500 "
-          >
-            <option disabled>Select a Value</option>
-            <option value="monthly">Monthly</option>
-            <option value="purchase">Purchase</option>
-          </select>
-        </div>
-      </section>
-      <div className="w-fit mx-auto">
-        <button
-          className="bg-teal-600 hover:bg-teal-700 px-20 py-2 rounded font-medium text-white "
-          onClick={handleSubmit}
-        >
-          Search
-        </button>
-      </div>
-      <section>
-        {formData.type
-          ? formData.type === "purchase"
-            ? expenses.map((data, idx) => <Purchase data={data} key={idx} />)
-            : expenses.map((data, idx) => <Expense data={data} key={idx} />)
-          : null}
+
+        <form className="my-8" action="">
+
+          <section className="grid grid-cols-1 md:grid-cols-3 max-w-5xl mx-auto gap-4">
+            <BranchSamitySelector callBackFn={setFormData} />
+            <div className="flex flex-col gap-1">
+              <label className="font-medium " htmlFor="payment_term">
+                Expense Type:
+              </label>
+              <select
+                onChange={handleChange}
+                name="type"
+                className=" input input-bordered input-sm hover:border-teal-500 "
+              >
+                <option disabled>--Select a Value--</option>
+                <option value="monthly">Monthly</option>
+                <option value="purchase">Purchase</option>
+              </select>
+            </div>
+          </section>
+
+          <div className="w-fit mx-auto">
+            <button
+              className="bg-teal-600 hover:bg-teal-700 px-20 py-2 rounded font-medium text-white "
+              onClick={handleSubmit}
+            >
+              Search
+            </button>
+          </div>
+          <section>
+            {formData.type
+              ? formData.type === "purchase"
+                ? expenses.map((data, idx) => <Purchase data={data} key={idx} />)
+                : expenses.map((data, idx) => <Expense data={data} key={idx} />)
+              : null}
+          </section>
+        </form>
       </section>
     </div>
   );
