87bb43eced80aa3db5943d278ce2fa3401a812e5
joyhasan08 <rubayedhasan235@gmail.com>
2024-05-18 19:39:32 +0600

    saving acc list done

--- src/pages/Saving Account/SavingAccountListDetails.jsx
+++ src/pages/Saving Account/SavingAccountListDetails.jsx
@@ -5,25 +5,58 @@ import { dateToString } from '../../utils/DateHelper';
 
 const SavingAccountPerUserDetails = ({ data }) => {
     console.log(data);
-    const { memberId, paymentTerm, periodOfTimeInMonths, perInstallment, profitPercentage, onMatureAmount, openingDate, matureDate, balance, createdAt, updatedAt, transactions } = data;
-    console.log(transactions);
+    const { memberId, paymentTerm, periodOfTimeInMonths, perInstallment, profitPercentage, onMatureAmount, openingDate, matureDate, balance, createdAt, updatedAt, memberDetails } = data;
+
+
 
     return (
         <div>
-            <h1 className='border-b-4 bg-teal-100 text-gray-600 text-lg pl-6 font-medium rounded-b-md pb-2 pt-1 flex items-center gap-2'><span><img className='w-8' src="./../../../public/Admin Dashboard icon/Total members.png" alt="" /></span> User Details</h1>
+            <h1 className='border-b-4  text-gray-600 text-lg pl-6 font-medium  pb-2 pt-1 flex items-center gap-2'><span><img className='w-8' src="./../../../public/Admin Dashboard icon/Total members.png" alt="" /></span> User Details</h1>
             <div className=' border-b-4 text-base font-medium tracking-wide divide-y-1 space-y-3 grid grid-cols-1 leading-6 p-6 py-3 bg-teal-50 rounded-b-md '>
-                <p className='flex items-center gap-2'><span><img className='w-4' src="/NGO Dashboard icon/Member.png" alt="" /></span> Member Name : <span className='font-normal pl-2'>Mr. Fokat </span></p>
+                <p className='flex items-center gap-2'><span><img className='w-4' src="/NGO Dashboard icon/Member.png" alt="" /></span> Member Name : <span className='font-normal pl-2'>{memberDetails.name} </span></p>
+                <p className='flex items-center gap-2'><span><img className='w-4' src="/NGO Dashboard icon/Member.png" alt="" /></span> Phone: <span className='font-normal pl-2'>{memberDetails.mobileNumber} </span></p>
                 <p className='flex items-center gap-2'> <img className='w-5' src="/NGO Dashboard icon/expense.png" alt="" />Payment Term: <span className='font-normal px-2 bg-green-500 text-white rounded'>{paymentTerm}</span></p>
                 <p className='flex items-center gap-2'> <img className='w-5' src="./../../../public/NGO Dashboard icon/report.png" alt="" />Period Of Months: <span className='font-normal text-white rounded px-2 bg-teal-500'>{periodOfTimeInMonths}</span></p>
-                <p className='flex items-center gap-2'> <img className='w-5' src="./../../../public/NGO Dashboard icon/payroll.png" alt="" />Per Installment: <span className='font-normal pl-2'>{perInstallment} $</span></p>
-                <p className='flex items-center gap-2'> <img className='w-5' src="./../../../public/NGO Dashboard icon/bank-building.png" alt="" />Balance: <span className='font-normal pl-2'>{balance} $</span></p>
+                <p className='flex items-center gap-2'> <img className='w-5' src="./../../../public/NGO Dashboard icon/payroll.png" alt="" />Per Installment: <span className='font-normal pl-2'>{perInstallment} </span></p>
+                <p className='flex items-center gap-2'> <img className='w-5' src="./../../../public/NGO Dashboard icon/bank-building.png" alt="" />Balance: <span className='font-normal pl-2'>{balance} </span></p>
                 <p className='flex items-center gap-2'> <img className='w-5' src="./../../../public/NGO Dashboard icon/manage-drawer-cash.png" alt="" />Profit : <span className='font-normal pl-2'>{profitPercentage} %</span></p>
                 <div className='flex flex-wrap  gap-4 border-t-2 pt-2'>
                     <p className='flex items-center gap-2 '><span><img width="22" height="22" src="https://img.icons8.com/ios/50/calendar--v1.png" alt="calendar--v1" /></span>On Mature Date : <span className='font-normal '>{dateToString(onMatureAmount)} </span></p>
                     <p className='flex items-center gap-2 '><span><img width="22" height="22" src="https://img.icons8.com/ios/50/calendar--v1.png" alt="calendar--v1" /></span>Opening Date : <span className='font-normal '>{dateToString(openingDate)} </span></p>
-                    <p className='flex items-center gap-2'><span><img width="22" height="22" src="https://img.icons8.com/ios/50/calendar--v1.png" alt="calendar--v1" /></span>Update Date : <span className='font-normal '>{dateToString(updatedAt)} </span></p>
+
                 </div>
             </div>
+        </div>
+    )
+}
+
+const TransactionsTable = ({ data }) => {
+    console.log(data);
+
+    const { date, amount, description, _id, } = data
+
+    return (
+        <div>
+            <tr className="grid grid-cols-3 text-xs md:text-base bg-gray-100  border-b-4   md:grid-cols-3 items-center w-full justify-between text-center py-3">
+                <td>{dateToString(date)}</td>
+                <td>{amount}</td>
+                <td className="hidden md:block">{description}</td>
+            </tr>
+        </div>
+    )
+}
+
+const WithdrawsTable = ({ data }) => {
+    console.log(data);
+
+    const { date, amount, _id } = data
+    return (
+        <div>
+            <tr className="grid grid-cols-2 text-xs md:text-base bg-gray-100  border-b-4   md:grid-cols-2 items-center w-full justify-between text-center py-3">
+                <td>{dateToString(date)}</td>
+                <td>{amount}</td>
+
+            </tr>
         </div>
     )
 }
@@ -41,6 +74,40 @@ const SavingAccountListDetails = () => {
             <section className='max-w-5xl mx-auto '>
                 <SavingAccountPerUserDetails data={data} />
             </section>
+            <div className='grid  md:grid-cols-2 '>
+                <section>
+                    <div className="md:m-8">
+                        <h1 className='md:text-lg md:font-medium mt-3 text-center '>Transactions Table</h1>
+                        <div className='divider'></div>
+                        <table className="w-full  ">
+                            <tr className="grid grid-cols-3  text-xs md:text-base bg-teal-700  py-4 text-white md:grid-cols-3 items-center justify-center gap-1 text-center">
+                                <th>Date</th>
+                                <th>Amount</th>
+                                <th>Description</th>
+                            </tr>
+                            {
+                                data.transactions ? data.transactions.map((data, idx) => <TransactionsTable data={data} key={idx} />) : null
+                            }
+                        </table>
+                    </div>
+                </section>
+                <section>
+                    <div className="md:m-8">
+                        <h1 className='md:text-lg md:font-medium mt-4 text-center '>Withdraws Table</h1>
+                        <div className='divider'></div>
+                        <table className="w-full  ">
+                            <tr className="grid grid-cols-2  text-xs md:text-base bg-teal-700  py-4 text-white md:grid-cols-2 items-center justify-center gap-1 text-center">
+                                <th>Date</th>
+                                <th>Amount</th>
+
+                            </tr>
+                            {
+                                data.withdraws ? data.withdraws.map((data, idx) => <WithdrawsTable data={data} key={idx} />) : null
+                            }
+                        </table>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 };
--- src/pages/Saving Account/SavingAccountListView.jsx
+++ src/pages/Saving Account/SavingAccountListView.jsx
@@ -11,6 +11,7 @@ const SavingAccountListView = ({ data }) => {
         onMatureAmount,
         openingDate,
         balance,
+        memberDetails
     } = data
     return (
         <div>
