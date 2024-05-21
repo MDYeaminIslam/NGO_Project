b152bda4fa94e456b3fa095492721aa0be54924c
joyhasan08 <rubayedhasan235@gmail.com>
2024-05-22 01:26:51 +0600

    leave application

--- src/component/EmployeeLeaveApplication.jsx
+++ src/component/EmployeeLeaveApplication.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+const initialState = {
+    days: null,
+    reason: '',
 
+};
 
 const EmployeeLeaveApplication = () => {
+    const [formData, setFormData] = useState(initialState);
+    const onChangeHandle = (e) => {
+        const { name, value } = e.target;
+        console.log(name, value);
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    }
+    console.log(formData);
     return (
         <div>
             <section>
                 <h1 className="text-xl font-bold text-start max-w-5xl mx-auto  pt-4 border-b-4 pb-2 ">
                     Leave Application
                 </h1>
-                <section className=" grid grid-cols-1 md:grid-cols-2 max-w-5xl mx-auto gap-4 my-8">
+                <section className=" px-4 grid grid-cols-1 md:grid-cols-1 max-w-5xl mx-auto gap-4 my-8">
                     <div className="flex flex-col gap-1">
                         <label className="font-medium" htmlFor="number_of_days">
                             Number of Days:
@@ -15,9 +27,10 @@ const EmployeeLeaveApplication = () => {
                         <input
 
                             className="input input-bordered input-sm  hover:border-teal-500"
-                            id="number_of_days"
+                            id="days"
                             type="number"
-                            name="number_of_days"
+                            name="days"
+                            onChange={onChangeHandle}
                         />
                     </div>
 
@@ -25,22 +38,20 @@ const EmployeeLeaveApplication = () => {
                         <label className="font-medium" htmlFor="reason">
                             Reason:
                         </label>
-                        <input
 
+                        <textarea
                             name="reason"
                             id="reason"
-                            type="text"
-                            className="input input-bordered input-sm  hover:border-teal-500  "
-                        />
+                            onChange={onChangeHandle}
+                            className="input input-bordered input-lg  hover:border-teal-500  "
+                            cols="30" rows="10">
+                        </textarea>
                     </div>
 
                 </section>
                 <div className="w-full flex flex-col md:flex-row justify-center  mt-8">
                     <button
-                        
-                        className="bg-teal-600 hover:bg-teal-700 px-20 py-2 rounded font-medium     text-white"
-                        
-                    >
+                        className="bg-teal-600 hover:bg-teal-700 px-20 py-2 rounded font-medium     text-white">
                         Submit
                     </button>
                 </div>
