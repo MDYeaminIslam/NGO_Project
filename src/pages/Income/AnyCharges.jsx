e988273ef27ac45ccaec21a4034dc5230a6e7424
joyhasan08 <rubayedhasan235@gmail.com>
2024-06-08 14:14:18 +0600

    add bank

--- src/pages/Income/AnyCharges.jsx
+++ src/pages/Income/AnyCharges.jsx
@@ -11,11 +11,18 @@ const initalState = {
 
 const AnyCharges = () => {
   const [date, setDate] = useState(new Date());
-  const { mutate, isPending } = useMutationHook(getIncome);
+  const [income, setIncome] = useState({})
+  const { mutate, isPending } = useMutationHook(getIncome, {
+    onSuccess: (data) => {
+      console.log(data.data);
+      setIncome(data.data)
+    }
+  });
   function handleSubmit(event) {
     event.preventDefault();
     mutate(date);
   }
+  console.log(income);
 
   return (
     <div>
@@ -23,19 +30,21 @@ const AnyCharges = () => {
         <IncomeNav />
       </section>
 
-      <div className="flex flex-col gap-1">
-        <label className="font-medium" htmlFor="membership_fee ">
-          Select Date:
-        </label>
-        <DatePicker
-          selected={date}
-          onChange={(date) => setDate(date)}
-          className="input input-bordered input-sm  hover:border-teal-500 w-full"
-          dateFormat="dd/MM/yyyy"
-          required
-        />
-        <button onClick={handleSubmit}>Search</button>
-      </div>
+      <section className="max-w-5xl mx-auto">
+        <div className="flex flex-col gap-1 mt-4">
+          <label className="font-medium" htmlFor="membership_fee ">
+            Select Date:
+          </label>
+          <DatePicker
+            selected={date}
+            onChange={(date) => setDate(date)}
+            className="input input-bordered input-sm  hover:border-teal-500 w-full"
+            dateFormat="dd/MM/yyyy"
+            required
+          />
+          <button className="btn btn-sm w-fit bg-teal-500 text-white hover:bg-teal-700 mx-auto mt-4" onClick={handleSubmit}>Search</button>
+        </div>
+      </section>
     </div>
   );
 };
